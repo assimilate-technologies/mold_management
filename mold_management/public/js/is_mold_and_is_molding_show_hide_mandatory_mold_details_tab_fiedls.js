9e475089c3cd74d3147e11ac49ae3287c7c9d34a
maheshndev <maheshnarsale@outlook.com>
2026-07-17 15:10:35 +0530

    fixed issue that cavity field is not visible

--- mold_management/public/js/is_mold_and_is_molding_show_hide_mandatory_mold_details_tab_fiedls.js
+++ mold_management/public/js/is_mold_and_is_molding_show_hide_mandatory_mold_details_tab_fiedls.js
@@ -97,7 +97,6 @@ frappe.ui.form.on("Item", {
 			"mould_details_section",
 			"mould_selection_table",
 			"cycle_time",
-			"cavity",
 			"gluing",
 			"drilling",
 			"def_1_hrs",
@@ -118,6 +117,7 @@ frappe.ui.form.on("Item", {
 			"std_pkg_box",
 			"other_operations",
 			"part_specification_section",
+			"cavity", // moved after Part Specification Section
 			"box_packing_section",
 			"req_poly_bags_sizes",
 			"density",
@@ -169,6 +169,9 @@ frappe.ui.form.on("Item", {
 			tabs.forEach((tab) => frm.set_df_property(tab, "hidden", 0));
 			// Show moulding fields
 			moulding_fields.forEach((field) => frm.set_df_property(field, "hidden", 0));
+            // Ensure cavity field is visible (explicitly set in case of naming issues)
+            frm.set_df_property("cavity", "hidden", 0);
+            frm.refresh_field("cavity");
 			// Mandatory table
 			frm.set_df_property("mould_selection_table", "reqd", 1);
 		} else if (frm.doc.is_mould_item) {

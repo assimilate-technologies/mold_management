3d9feeca6964f127204b5668dfea67f61c26403b
maheshndev <maheshnarsale@outlook.com>
2026-07-22 18:26:09 +0530

    fixed fields issues

--- mold_management/public/js/is_mold_and_is_molding_show_hide_mandatory_mold_details_tab_fiedls.js
+++ mold_management/public/js/is_mold_and_is_molding_show_hide_mandatory_mold_details_tab_fiedls.js
@@ -1,60 +1,4 @@
-// frappe.ui.form.on('Item', {
-//     refresh(frm) {
-//         frm.trigger("toggle_fields");
-//     },
 
-//     is_mould_item(frm) {
-//         frm.trigger("toggle_fields");
-//     },
-
-//     is_moulding(frm) {
-//         frm.trigger("toggle_fields");
-//     },
-
-//     toggle_fields(frm) {
-//         let mould_item = frm.doc.is_mould_item;
-//         let moulding = frm.doc.is_moulding;
-
-//         // Fields to show/hide and make mandatory
-//         let fields = [
-//             "shape", "material_type", "no_of_cavity", "side_cores",
-//             "side_cores_qty", "hot_runner_system", "cold_runner_system",
-//             "tool_life", "total_shots"
-//         ];
-
-//         if (mould_item) {
-//             // 1️⃣ If is_mould_item = checked
-//             frm.set_df_property("mould_selection_table", "reqd", 1);
-
-//             fields.forEach(f => {
-//                 frm.set_df_property(f, "hidden", 1);
-//                 frm.set_df_property(f, "reqd", 0);
-//             });
-
-//             frm.set_df_property("mould_selection_table", "hidden", 0);
-//         }
-//         else if (moulding) {
-//             // 2️⃣ If is_moulding = checked
-//             frm.set_df_property("mould_selection_table", "hidden", 1);
-//             frm.set_df_property("mould_selection_table", "reqd", 0);
-
-//             fields.forEach(f => {
-//                 frm.set_df_property(f, "hidden", 0);
-//                 frm.set_df_property(f, "reqd", 1);
-//             });
-//         }
-//         else {
-//             // 3️⃣ If none selected -> Reset
-//             frm.set_df_property("mould_selection_table", "hidden", 0);
-//             frm.set_df_property("mould_selection_table", "reqd", 0);
-
-//             fields.forEach(f => {
-//                 frm.set_df_property(f, "hidden", 0);
-//                 frm.set_df_property(f, "reqd", 0);
-//             });
-//         }
-//     }
-// });
 
 frappe.ui.form.on("Item", {
 	refresh(frm) {
@@ -94,16 +38,16 @@ frappe.ui.form.on("Item", {
 		];
 
 		const moulding_tabs = [
+			"mould_details_tab",
 			"rework_and_checking_details_tab",
 			"part_specification_tab",
 			"packing_details_tab",
 		];
 
 		const moulding_fields = [
-			"mould",
+			"mould_selection_table",
+			"mould_details_section",
 			"cycle_time",
-			"gluing",
-			"drilling",
 			"def_1_hrs",
 			"checking_1_hrs",
 			"engraving_in_1hrs",
@@ -118,26 +62,32 @@ frappe.ui.form.on("Item", {
 			"standard_pkg_of_polybag",
 			"cavity",
 			"pcs_wt",
-			"bending",
-			"shift_prod",
 			"std_pkg_box",
-			"other_operations",
 			"part_specification_section",
 			"box_packing_section",
 			"req_poly_bags_sizes",
-			"clipping",
 		];
 
 		const moulding_required_fields = [
+			"mould_selection_table",
 			"cycle_time",
+			"def_1_hrs",
+			"checking_1_hrs",
+			"engraving_in_1hrs",
+			"runner_wt",
+			"cutting_1_hrs",
+			"gross_wt",
+			"req_total_box",
+			"shot_wt",
+			"req_boxsizes",
+			"standard_pkg_of_polybag",
 			"cavity",
 			"pcs_wt",
-			"runner_wt",
-			"shot_wt",
-			"gross_wt",
+			"std_pkg_box",
+			"req_poly_bags_sizes",
 		];
 
-		const mould_item_visible_fields = [
+		const mould_item_fields = [
 			"shape",
 			"material_type",
 			"no_of_cavity",
@@ -148,7 +98,14 @@ frappe.ui.form.on("Item", {
 			"tool_life",
 			"total_shots",
 			"total_lifecycle_shot",
-			"mould_selection_table",
+			"mould_details_section",
+			"drilling",
+			"shift_prod",
+			"gluing",
+			"bending",
+			"clipping",
+			"mould",
+			"other_operations",
 			"mould_name",
 			"mould_ty",
 		];
@@ -164,37 +121,70 @@ frappe.ui.form.on("Item", {
 			"tool_life",
 			"total_shots",
 			"total_lifecycle_shot",
-			"mould_selection_table",
+			"drilling",
+			"shift_prod",
+			"gluing",
+			"bending",
+			"clipping",
+			"mould",
+			"other_operations",
 			"mould_name",
 			"mould_ty",
 		];
 
-		const all_monitored_fields = [...moulding_fields, ...mould_item_visible_fields];
+		const mould_item_tabs = [
+			"mould_details_tab",
+			"part_specification_tab",
+		];
 
-		// Reset visibility and mandatory status for all monitored fields and tabs
-		all_tabs.forEach((tab) => frm.set_df_property(tab, "hidden", 1));
-		all_monitored_fields.forEach((field) => {
+		const all_monitored_fields = Array.from(new Set([
+			...moulding_fields,
+			...mould_item_fields,
+		]));
+
+		const hideTab = (tab) => {
+			frm.set_df_property(tab, "hidden", 1);
+			frm.toggle_display(tab, false);
+		};
+
+		const showTab = (tab) => {
+			frm.set_df_property(tab, "hidden", 0);
+			frm.toggle_display(tab, true);
+		};
+
+		const hideField = (field) => {
 			frm.set_df_property(field, "hidden", 1);
 			frm.set_df_property(field, "reqd", 0);
-		});
+			frm.toggle_display(field, false);
+		};
+
+		const showField = (field) => {
+			frm.set_df_property(field, "hidden", 0);
+			frm.toggle_display(field, true);
+		};
+
+		const setRequired = (field) => {
+			frm.set_df_property(field, "reqd", 1);
+		};
+
+		all_tabs.forEach(hideTab);
+		all_monitored_fields.forEach(hideField);
 
 		if (frm.doc.is_moulding) {
-			moulding_tabs.forEach((tab) => frm.set_df_property(tab, "hidden", 0));
-			moulding_fields.forEach((field) => frm.set_df_property(field, "hidden", 0));
-			moulding_required_fields.forEach((field) => {
-				frm.set_df_property(field, "reqd", 1);
-			});
+			moulding_tabs.forEach(showTab);
+			moulding_fields.forEach(showField);
+			moulding_required_fields.forEach(setRequired);
 		} else if (frm.doc.is_mould_item) {
-			frm.set_df_property("mould_details_tab", "hidden", 0);
-			frm.set_df_property("mould_details_section", "hidden", 0);
-			mould_item_visible_fields.forEach((field) => {
-				frm.set_df_property(field, "hidden", 0);
-			});
-			mould_item_required_fields.forEach((field) => {
-				frm.set_df_property(field, "reqd", 1);
-			});
+			mould_item_tabs.forEach(showTab);
+			mould_item_fields.forEach(showField);
+			mould_item_required_fields.forEach(setRequired);
+			frm.set_df_property("mould_selection_table", "hidden", 1);
+			frm.set_df_property("mould_selection_table", "reqd", 0);
+			frm.toggle_display("mould_selection_table", false);
 		} else if (frm.doc.other_than_mould_or_moulding) {
-			// All tabs and fields remain hidden and unmandatory (already reset above)
+			frm.set_df_property("mould_selection_table", "hidden", 1);
+			frm.set_df_property("mould_selection_table", "reqd", 0);
+			frm.toggle_display("mould_selection_table", false);
 		}
 
 		frm.refresh_fields();
